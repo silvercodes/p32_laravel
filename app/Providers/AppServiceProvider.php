8409b50d283846b054971fc5e1114b0a03d2afad
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-03-30 18:30:57 +0300

    add: directives, composers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,13 +3,16 @@
 namespace App\Providers;
 
 
+use App\Composers\RoleComposer;
 use App\Models\Post;
 use App\Services\IconService;
 use App\Services\LoggerService;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -52,5 +55,19 @@ class AppServiceProvider extends ServiceProvider
             return Post::where('slug', $value)->first() ??
                 Post::findOrFail($value);
         });
+
+        ///////
+        Blade::directive('isAdmin', function ($expression) {
+            return "<?php if(Auth::check() && Auth::user()->role === 'admin'): ?>";
+        });
+
+        ///////
+        Blade::directive('endIsAdmin', function ($expression) {
+            return "<?php endif; ?>";
+        });
+
+        ///////
+        View::composer('*', RoleComposer::class);
+
     }
 }
